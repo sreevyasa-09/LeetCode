c4cb3d7c79358b963ea06440eb989d30bc809c38
sreevyasa-09 <sreevyasa119@gmail.com>
2026-03-21 18:59:04 +0530

    Time: 2 ms (21.91%), Space: 43.8 MB (56.11%) - LeetHub

--- 0088-merge-sorted-array/0088-merge-sorted-array.java
+++ 0088-merge-sorted-array/0088-merge-sorted-array.java
@@ -6,6 +6,19 @@ class Solution {
             nums1[u] = nums2[k];
             k++;
         }
-        Arrays.sort(nums1);
+        sort(nums1);
     }
+    public void sort(int[] nums1){
+        int n = nums1.length;
+    for(int i = 0; i < n; i++){
+            int min = i;
+            for(int j = i +1; j < n; j++){
+                if(nums1[j] < nums1[min])
+                min = j;
+            }
+            int temp = nums1[min];
+        nums1[min] = nums1[i];
+        nums1[i] = temp;
+        }
+}
 }
