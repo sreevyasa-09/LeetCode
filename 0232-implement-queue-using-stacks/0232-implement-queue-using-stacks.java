7247837cf36ff5c3c93a9b9ecd76f7710d2dfff5
sreevyasa-09 <sreevyasa119@gmail.com>
2026-04-09 19:21:06 +0530

    Time: 0 ms (100%), Space: 42.5 MB (81.96%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -1,51 +1,35 @@
-import java.util.Stack;
-
 class MyQueue {
 
     Stack<Integer> s1;
     Stack<Integer> s2;
-
     public MyQueue() {
         s1 = new Stack<>();
         s2 = new Stack<>();
     }
     
     public void push(int x) {
+        while(!s1.isEmpty()){
+            s2.add(s1.pop());
+        }
         s1.push(x);
+        while(!s2.isEmpty()){
+            s1.push(s2.pop());
+        }
     }
     
     public int pop() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
-            }
-        }
-
-        if (!s2.isEmpty()) {
-            return s2.pop();
-        }
-
-        return -1;
+        return s1.pop();
     }
     
     public int peek() {
-        if (s2.isEmpty()) {
-            while (!s1.isEmpty()) {
-                s2.push(s1.pop());
-            }
-        }
-
-        if (!s2.isEmpty()) {
-            return s2.peek();
-        }
-
-        return -1;
+        return s1.peek();
     }
     
     public boolean empty() {
-        return s1.isEmpty() && s2.isEmpty();
+        return (s1.isEmpty() && s2.isEmpty());
     }
 }
+
 /**
  * Your MyQueue object will be instantiated and called as such:
  * MyQueue obj = new MyQueue();
